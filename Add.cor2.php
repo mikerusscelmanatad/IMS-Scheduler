8a7665ff08fbf470386879113bc071bc49973290
ims7 <imsmkt@ims7.com>
2023-12-27 17:32:00 +0800

    commited

--- Add.cor2.php
+++ Add.cor2.php
@@ -27,7 +27,7 @@ if (isset($_POST['update'])) {
 	$student_course = mysqli_real_escape_string($connect, $_POST['student_course']);
 	$student_level = mysqli_real_escape_string($connect, $_POST['student_level']);
 	
-	 $query = "UPDATE subject SET `student_id`='$student_name', `student_name`='$student_name', `student_course`='$student_course', `student_level`='$student_level', `updated_at`=NOW() WHERE `student_id`='$student_id'";
+	 $query = "UPDATE subject SET `student_id`='$student_id', `student_name`='$student_name', `student_course`='$student_course', `student_level`='$student_level', `updated_at`=NOW() WHERE `student_id`='$student_id'";
     
 	$result = mysqli_query($connect, $query);
 }
