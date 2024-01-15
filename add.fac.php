540703013c21593f327d0059af59cf9771322393
ims7 <imsmkt@ims7.com>
2024-01-15 11:48:06 +0800

    adding the add teacher and the designated room

--- add.fac.php
+++ add.fac.php
@@ -2,17 +2,25 @@
 
 require 'databaasee.php';
 
-$Faculty_Name = $_POST['falname'];
-$Designation = $_POST['Designation'];
+$faculty_name = $_POST['falname'];
+$designation = $_POST['Designation'];
 $room = $_POST['room'];
 
-$sql = "INSERT INTO faculty (Faculty_Name, Designation, room) VALUES ('$Faculty_Name', '$Designation', '$room')";
 
-if (!mysqli_query($connect, $sql)) {
-	echo 'not inserted';
-} else {
-	echo '<script type="text/javascript">
-                      alert("New Teacher Added!");
-                         location="addteacher.php";
-                           </script>';
+$findRoom = "SELECT `room_id`,`room` FROM `rooms` WHERE `room`=$room";
+$findRoomResult = mysqli_query($connect, $findRoom);
+
+while ($row = mysqli_fetch_array($findRoomResult)) {
+	$room = $row["room_id"];
+
+     $sql = "INSERT INTO faculty (faculty_name, designation, room) VALUES ('$faculty_name', '$designation', '$room')";
+
+    if (!mysqli_query($connect, $sql)) {
+      echo 'not inserted';
+    } else {
+      echo '<script type="text/javascript">
+                          alert("New Teacher Added!");
+                            location="addteacher.php";
+                              </script>';
+    }
 }
